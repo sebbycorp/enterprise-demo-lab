f5bb698afa828c39a5ae9e706498cf7068b6332c
sebbycorp <sebastian@maniak.io>
2025-07-11 08:24:21 -0400

    asdf

--- lab-enterprise/services/temp.sh
+++ lab-enterprise/services/temp.sh
@@ -8,7 +8,7 @@ sudo ip route del 10.1.0.0/16 via 10.1.11.1 dev team0.111; \
 sudo ip route add 10.1.0.0/16 via 10.1.11.10 dev team0.111; \
 '
 
-docker exec -it clab-s2-DC1_client1 /bin/sh -c "$ROUTE1"
-docker exec -it clab-s2-DC1_client2 /bin/sh -c "$ROUTE2"
-docker exec -it clab-s2-DC1_client3 /bin/sh -c "$ROUTE1"
-docker exec -it clab-s2-DC1_client4 /bin/sh -c "$ROUTE2"
+docker exec -it clab-s2-dc1_client1 /bin/sh -c "$ROUTE1"
+docker exec -it clab-s2-dc1_client2 /bin/sh -c "$ROUTE2"
+docker exec -it clab-s2-dc1_client3 /bin/sh -c "$ROUTE1"
+docker exec -it clab-s2-dc1_client4 /bin/sh -c "$ROUTE2"
